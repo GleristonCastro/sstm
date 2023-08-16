6c6b747d288ee32decb60d9674da01c35cbcbf47
Gleriston <xpanium@gmail.com>
2023-08-16 13:39:51 -0300

    add login scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,32 @@ const includes = (sections) => {
 window.onload = function () {
   includes([
     'header',
+    'login',
     'footer',
   ]);
+
+  setTimeout(() => {
+    const signUp = document.getElementById('sign-up');
+    const signIn = document.getElementById('sign-in');
+    const loginIn = document.getElementById('login-in');
+    const loginUp = document.getElementById('login-up');
+
+  signUp.addEventListener('click', () => {
+    loginIn.classList.remove('block');
+    loginUp.classList.remove('none');
+
+    loginIn.classList.toggle('none');
+    loginUp.classList.toggle('block');
+  });
+
+  signIn.addEventListener('click', () => {
+    loginIn.classList.remove('none');
+    loginUp.classList.remove('block');
+
+    loginIn.classList.toggle('block');
+    loginUp.classList.toggle('none');
+  });
+
+  }, 810);
+
 }
