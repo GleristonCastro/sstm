5b420b4c5ce71b7bfca3717e9b98babf4f263303
Gleriston <xpanium@gmail.com>
2023-08-16 16:17:52 -0300

    adding javascript from header in project

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,21 +28,34 @@ window.onload = function () {
     const loginIn = document.getElementById('login-in');
     const loginUp = document.getElementById('login-up');
 
-  signUp.addEventListener('click', () => {
-    loginIn.classList.remove('block');
-    loginUp.classList.remove('none');
+    signUp.addEventListener('click', () => {
+      loginIn.classList.remove('block');
+      loginUp.classList.remove('none');
 
-    loginIn.classList.toggle('none');
-    loginUp.classList.toggle('block');
-  });
+      loginIn.classList.toggle('none');
+      loginUp.classList.toggle('block');
+    });
 
-  signIn.addEventListener('click', () => {
-    loginIn.classList.remove('none');
-    loginUp.classList.remove('block');
+    signIn.addEventListener('click', () => {
+      loginIn.classList.remove('none');
+      loginUp.classList.remove('block');
 
-    loginIn.classList.toggle('block');
-    loginUp.classList.toggle('none');
-  });
+      loginIn.classList.toggle('block');
+      loginUp.classList.toggle('none');
+    });
+
+    const showMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu')
+
+        toggle.classList.toggle('show-icon')
+      })
+    }
+
+    showMenu('nav-toggle', 'nav-menu')
 
   }, 810);
 
